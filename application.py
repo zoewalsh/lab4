be710fc4ac2af18897ed188978733ab9354dd068
zoewalsh <zoeswalsh@gmail.com>
2021-03-12 15:44:39 -0700

    remove unnecessary commented lines

--- application.py
+++ application.py
@@ -18,8 +18,6 @@ if sys.platform.lower() == "win64":
 # default route, no data is passed to index
 @app.route("/", methods=['POST', 'GET'])
 def index():
-    #SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    #json_url = os.path.join(SITE_ROOT, "static", "Community_Services.geojson")
     url_services = request.url_root + 'static/Community_Services.geojson'
     url_schools = request.url_root + 'static/School_Locations.geojson'
     return render_template("index.html", url_services=url_services, url_schools=url_schools)
